eecccfbfdc49551607eaf4026db540d4fd94318a
Rebecca Mwangi <rebzhaki@gmail.com>
2025-11-28 01:03:15 +0300

    include footer &&  responsive cart on mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import './App.css'
-import { Routes, BrowserRouter as Router, Route } from 'react-router-dom'
-import NavbarSection from './components/navbar'
-import MenuList from './components/menu/menuList'
-import { SearchProvider } from './context/searchContext'
-
+import "./App.css";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import NavbarSection from "./components/navbar";
+import MenuList from "./components/menu/menuList";
+import { SearchProvider } from "./context/searchContext";
+import React, { useEffect, useState } from "react";
+import { Image } from "react-bootstrap";
+import logo from "/bite-lite-logo.png";
+import Footer from "./components/footer";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500); 
 
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        className="d-flex flex-column justify-content-center align-items-center vh-100 loadingScreen"
+      >          
+        <Image
+          src={logo}
+          className="img-fluid"
+          style={{ maxWidth: "100px", height: "auto" }}
+          alt="BiteLite Logo"
+          roundedCircle
+        />
+      </div>
+    );
+  }
   return (
     <div className="App">
       <SearchProvider>
-      <NavbarSection />
-      <Router>
-        <Routes>
-          <Route path="/" element={<MenuList />} />
-        </Routes>
-
-      </Router>
+        <NavbarSection />
+        <Router>
+            <Routes>
+              <Route path="/" element={<MenuList />} />
+            </Routes>
+        </Router>
+        <Footer />
       </SearchProvider>
-   
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,79 @@
+import { Col, Image, Row } from "react-bootstrap";
+import logo from "/bite-lite-logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light text-center text-lg-start mt-4">
+      <Row>
+        <Col className="text-center p-3" style={{ backgroundColor: "#FFF3EA" }}>
+          <a
+            href=""
+            className="text-dark fw-bolder"
+            style={{ textDecoration: "none" }}
+          >
+            <Image
+          src={logo}
+          className="img-fluid"
+          style={{ maxWidth: "100px", height: "auto" }}
+          alt="BiteLite Logo"
+          roundedCircle
+       />
+            BiteLite
+          </a>
+        </Col>
+        <Col className="text-center p-3" style={{ backgroundColor: "#FFF3EA" }}>
+          <a
+            href=""
+            className="text-dark"
+            style={{ textDecoration: "none" }}
+          >
+            Terms of Service
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-dark"
+            style={{ textDecoration: "none" }}
+          >
+            Privacy Policy
+          </a>
+        </Col>
+        <Col className="text-center p-3" style={{ backgroundColor: "#FFF3EA" }}>
+          <a
+            href=""
+            className="text-dark"
+            style={{ textDecoration: "none" }}
+          >
+            Contact Us
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-dark"
+            style={{ textDecoration: "none" }}
+          >
+            Support
+          </a>
+          <br />
+          <a
+            href=""
+            className="text-dark mt-2"
+            style={{ textDecoration: "none" }}
+          >
+            FAQ
+          </a>
+
+        </Col>
+
+      </Row>
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "#FFF3EA" }}
+      >
+        © {new Date().getFullYear()} BiteLite. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
